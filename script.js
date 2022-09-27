735091ad89382b74c1542f6d5868a7e05d273448
Kellon Ko <kellondavidko@gmail.com>
2022-09-27 13:04:35 -0700

    add function to add book to library

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
+// Global Varaibles
+const currentDisplay = document.querySelector(".currentDisplay");
+currentDisplay.innerHTML = "";
+
 // Data Inputs
 
-class book {
-  bookConstructor(title = "", author = "", page = "0", read = false) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.read = read;
-  }
+let myLibrary = [
+  {
+    title: "test",
+    author: "test",
+    page: "123",
+    read: true,
+  },
+  {
+    title: "test2",
+    author: "test",
+    page: "123",
+    read: false,
+  },
+  {
+    title: "test3",
+    author: "test",
+    page: "143",
+    read: true,
+  },
+];
+
+function book(title, author, page, read) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.read = read;
 }
 
-class library {
-  bookConstructor() {
-    this.allBooks = [];
-  }
+function addBookToLibrary() {
+  let a = document.getElementById("formTitle").value;
+  let b = document.getElementById("formAuthor").value;
+  let c = document.getElementById("formPage").value;
+  let d = document.getElementById("formRead").checked;
 
-  addBook(newBook) {
-    if (!this.isLibrary(newBook)) {
-      this.allBooks.push(newBook);
-    }
-  }
-
-  delBook(title) {
-    this.allBooks = this.books.filter((book) => book.title !== title);
-  }
-  isLibrary(newBook) {
-    return this.allBooks.some((book) => book.title === newBook.title);
+  if (a !== "" && b !== "" && c !== "") {
+    myLibrary.push(new book(a, b, c, d));
   }
 }
-
-const completeLibrary = new library();
